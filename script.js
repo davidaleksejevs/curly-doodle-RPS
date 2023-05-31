6b38228d3e9ad00c565a50529353a32fed36e379
David <76690633+davidaleksejevs@users.noreply.github.com>
2023-05-31 12:24:32 +0100

    made a game function that play 5 round with the person that puts game() in console

--- script.js
+++ script.js
@@ -5,15 +5,36 @@ function getComputerChoice(){
 }
 function play(playerSelection){
     const playerChoice = playerSelection.toLowerCase();
-    const computerChoice = getComputerChoice;
+    const computerChoice = getComputerChoice();
     if (playerChoice === computerChoice){
-        return `It's a tie! Both you and computer picked ${playerChoice}`
+        return `It's a tie! Both you and computer picked ${playerChoice}`;
     }else if(
-
+       (playerChoice === `rock` && computerChoice === `scissors`) || 
+       (playerChoice === `scissors` && computerChoice === `paper`) || 
+       (playerChoice === `paper` && computerChoice === `rock`)
     ){
-
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    }else {
+        return `You Lose! ${computerChoice} beats ${playerChoice}`;
+}}
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = prompt("Enter your choice (rock, paper, or scissors):");
+        const result = play(playerChoice);
+        console.log(result);
+        if (result.startsWith("You Win!")) {
+            playerScore++;
+        } else if (result.startsWith("You Lose!")) {
+            computerScore++;
+        }
     }
-    
-
+    if (playerScore > computerScore) {
+        console.log(`You won the game! The score was ${playerScore} to ${computerScore}.`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lost the game. The score was ${computerScore} to ${playerScore}.`);
+    } else {
+        console.log(`The game was a tie, with a score of ${playerScore} to ${computerScore}.`);
+    }
 }
-console.log(play("rock"));
