825c07c6afee811aebadb91437947fdf5f200cf0
David <76690633+davidaleksejevs@users.noreply.github.com>
2023-05-31 11:54:09 +0100

    spelling error in return

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
     const choice = ["Rock","Paper","Scissors"];
     const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
+    return choice[randomIndex];
 }
