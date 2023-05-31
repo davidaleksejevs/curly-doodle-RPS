6422701e126b9a62b44d19b5457bca9674cc67e2
David <76690633+davidaleksejevs@users.noreply.github.com>
2023-05-31 12:08:00 +0100

    added a function called play() that will be the logic to play one round of rock paper scissors

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function getComputerChoice(){
-    const choice = ["Rock","Paper","Scissors"];
+    const choice = ["rock","paper","scissors"];
     const randomIndex = Math.floor(Math.random() * 3);
     return choice[randomIndex];
 }
+function play(playerSelection){
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = getComputerChoice;
+    if (playerChoice === computerChoice){
+        return `It's a tie! Both you and computer picked ${playerChoice}`
+    }else if(
+
+    ){
+
+    }
+    
+
+}
+console.log(play("rock"));
